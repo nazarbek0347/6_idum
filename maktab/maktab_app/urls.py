eefcf952de115a0ed24f495f2f1b6dcabf831c1c
nazarbek0347 <matyoqubovn123@gmail.com>
2024-10-07 21:09:44 +0500

    'updated'

--- maktab/maktab_app/urls.py
+++ maktab/maktab_app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import contact,home,blog,about
 
 urlpatterns = [
-    path('',home,name='homePage')
+    path('',home,name='homePage'),
+    path('contact',contact,name='contactPage'),
+    path('blog',blog,name='blogPage'),
+    path('about',about,name='aboutPage')
 ]
--- maktab/maktab_app/views.py
+++ maktab/maktab_app/views.py
@@ -4,6 +4,34 @@ from django.shortcuts import redirect
 
 # Create your views here.
 
+bot = telebot.TeleBot("7835800080:AAEp9JjZyMjTAhU2Cptdjb51w57Oilvdc9s", parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
+
 
 def home(request):
     return render(request,'6-IDUM.html')
+
+def contact(request):
+    name = request.GET.get("name")
+    number = request.GET.get("number")
+    email = request.GET.get("email")
+    text = request.GET.get("message")
+
+
+    if name is not None:
+        message = f""" 
+        massage:
+        name: {name}
+        phone: {number}
+        email: {email}
+        text: {text}
+        """
+        bot.send_message(7189884017, message)
+        return redirect('contactPage')
+
+    return render(request, 'contact.html')
+
+def blog(request):
+    return render(request,'blog.html')
+
+def about(request):
+    return render(request,'about.html')
